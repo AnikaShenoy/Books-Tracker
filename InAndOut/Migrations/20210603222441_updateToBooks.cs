182c009de578b286398aa08efbfc0487c7113d1e
AnikaShenoy <anika.shenoy@mail.utoronto.ca>
2021-06-03 23:41:56 -0400

    Add project files.

---
+++ InAndOut/Migrations/20210603222441_updateToBooks.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace InAndOut.Migrations
+{
+    public partial class updateToBooks : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Borrower",
+                table: "Items");
+
+            migrationBuilder.RenameColumn(
+                name: "Lender",
+                table: "Items",
+                newName: "Title");
+
+            migrationBuilder.RenameColumn(
+                name: "ItemName",
+                table: "Items",
+                newName: "Genre");
+
+            migrationBuilder.AddColumn<float>(
+                name: "Rating",
+                table: "Items",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Rating",
+                table: "Items");
+
+            migrationBuilder.RenameColumn(
+                name: "Title",
+                table: "Items",
+                newName: "Lender");
+
+            migrationBuilder.RenameColumn(
+                name: "Genre",
+                table: "Items",
+                newName: "ItemName");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Borrower",
+                table: "Items",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
---
+++ InAndOut/Models/Item.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace InAndOut.Models
+{
+    public class Item
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [Range(0, 5, ErrorMessage = "Rating must be between 0 and 5!")]
+        public float Rating { get; set; }
+        [Required]
+        public string Genre { get; set; }
+        [DisplayName("Title")]
+        [Required]
+        public string Title{ get; set; }
+
+    }
+}
